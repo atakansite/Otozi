e3cd5ada459110d8864fbb1108d54d8dbff6e04c
atakansite <atakansite77@gmail.com>
2018-06-06 15:15:05 +0300

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -8,10 +8,7 @@ from django.conf.urls import url, include
 urlpatterns = [
     path('', views.index, name='index'),
     path('chat', views.chat_view, name='chats'),
-    path('chat/<int:sender>/<int:receiver>', views.message_view, name='chat'),
-    path('api/messages/<int:sender>/<int:receiver>', views.message_list, name='message-detail'),
     path('api/messages', views.message_list, name='message-list'),
-    path('api/users/<int:pk>', views.user_list, name='user-detail'),
     path('api/users', views.user_list, name='user-list'),
     path('logout', logout, {'next_page': 'index'}, name='logout'),
     path('register', views.register_view, name='register'),
